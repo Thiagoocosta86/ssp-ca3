437f58e66dd85f44f636000098d176ca9a514e80
Thiagoocosta86 <thiagoocosta86@gmail.com>
2022-01-06 18:41:51 +0000

    included get/html in line 15

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const router = express(),
 
 router.use(express.static(path.resolve(__dirname,'views')));
 
-router.get('/', function (req, res) {
+router.get('/get/html', function (req, res) {
 
                 res.writeHead(200, { 'Content-Type': 'text/html' });
 
