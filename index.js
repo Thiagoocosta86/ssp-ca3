c5e051725b6fbad4f54c763ff5098e59815e63b2
Thiagoocosta86 <thiagoocosta86@gmail.com>
2022-01-06 21:50:16 +0000

    included submit and delete functions on the page.

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const router = express(),
       server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname,'views')));
+router.use(express.urlencoded({extended: true}));
+router.use(express.json());
+
+// Function to read in XML file and convert it to JSON
+function XMLtoJSON(filename, cb) {
+        var filepath = path.normalize(path.join(__dirname, filename));
+        fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+          if (err) throw (err);
+          xml2js.parseString(xmlStr, {}, cb);
+        });
+    };
+      
+      //Function to convert JSON to XML and save it
+    function JSONtoXML(filename, obj, cb) {
+        var filepath = path.normalize(path.join(__dirname, filename));
+        var builder = new xml2js.Builder();
+        var xml = builder.buildObject(obj);
+        fs.unlinkSync(filepath);
+        fs.writeFile(filepath, xml, cb);
+    };
 
 router.get('/get/html', function (req, res) {
 
@@ -35,6 +55,56 @@ router.get('/get/html', function (req, res) {
                 res.end(result.toString());
 
 });
+
+router.post('/post/json', function (req, res) {
+
+        function appendJSON(obj) {
+    
+            console.log(obj)
+    
+            XMLtoJSON('2020005CarSales.xml', function (err, result) {
+                if (err) throw (err);
+                
+                result.root.section[obj.sec_n].entry.push({'item': obj.item, 'price': obj.price});
+    
+                console.log(JSON.stringify(result, null, "  "));
+    
+                JSONtoXML('2020005CarSales.xml', result, function(err){
+                    if (err) console.log(err);
+                });
+            });
+        };
+    
+        appendJSON(req.body);
+    
+        res.redirect('back');
+    
+    });
+    
+    router.post('/post/delete', function (req, res) {
+    
+        function deleteJSON(obj) {
+    
+            console.log(obj)
+    
+            XMLtoJSON('2020005CarSales.xml', function (err, result) {
+                if (err) throw (err);
+                
+                delete result.root.section[obj.section].entry[obj.entree];
+    
+                console.log(JSON.stringify(result, null, "  "));
+    
+                JSONtoXML('2020005CarSales.xml', result, function(err){
+                    if (err) console.log(err);
+                });
+            });
+        };
+    
+        deleteJSON(req.body);
+    
+        res.redirect('back');
+    
+    });
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
         const addr = server.address();
